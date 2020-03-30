e1d1ed51d13e209a38bfa0cdd480b38ad40a836f
Horacio Correia <rmcorre@gmail.com>
2020-03-30 19:26:27 +0000

    singleTable implementation: Implement Boat, Car, Vehicle, and Main classes

--- src/main/java/singleTable/Boat.java
+++ src/main/java/singleTable/Boat.java
@@ -1,4 +1,41 @@
 package singleTable;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity(name = "BoatSingleTable")
+@DiscriminatorValue("boat")
 public class Boat extends Vehicle {
+
+   private Integer maxSpeed;
+   private Integer gears;
+   private Integer engines;
+
+   public Boat() {
+
+   }
+
+   public Integer getMaxSpeed() {
+      return maxSpeed;
+   }
+
+   public void setMaxSpeed(Integer maxSpeed) {
+      this.maxSpeed = maxSpeed;
+   }
+
+   public Integer getGears() {
+      return gears;
+   }
+
+   public void setGears(Integer gears) {
+      this.gears = gears;
+   }
+
+   public Integer getEngines() {
+      return engines;
+   }
+
+   public void setEngines(Integer engines) {
+      this.engines = engines;
+   }
 }
--- src/main/java/singleTable/Car.java
+++ src/main/java/singleTable/Car.java
@@ -1,4 +1,40 @@
 package singleTable;
 
+import javax.persistence.*;
+
+@Entity(name = "CarSingleTable")
+@DiscriminatorValue("car")
 public class Car extends Vehicle {
+
+   private Integer maxSpeed;
+   private Integer gears;
+   private Integer engines;
+
+   public Car() {
+
+   }
+
+   public Integer getMaxSpeed() {
+      return maxSpeed;
+   }
+
+   public void setMaxSpeed(Integer maxSpeed) {
+      this.maxSpeed = maxSpeed;
+   }
+
+   public Integer getGears() {
+      return gears;
+   }
+
+   public void setGears(Integer gears) {
+      this.gears = gears;
+   }
+
+   public Integer getEngines() {
+      return engines;
+   }
+
+   public void setEngines(Integer engines) {
+      this.engines = engines;
+   }
 }
